14bdbcc952340a594aaceb1ca117c5145b115f61
Sreejarai123 <Sreeja.t.rai@gmail.com>
2023-10-20 08:59:54 -0700

    .studdybuddyupdated

--- model/study_buddy.py
+++ model/study_buddy.py
@@ -1,29 +1,55 @@
- # a setter function, allows name to be updated after initial object creation
-    @name.setter
-    def name(self, name):
-        self._name = name
-Create (C):
-newQuestion: A variable to store a new question being created.
-newAnswer: A variable to store the corresponding answer for the new question.
-newTopic: The topic or category to which the new question belongs.
+from flask import Flask, request, jsonify
 
-Read (R):
+app = Flask(__name__)
 
-questionsList: An array or list to store existing questions.
-answersList: An array or list to store corresponding answers.
-topicsList: An array or list to store the topics/categories of the questions.
+flashcards = []
 
-Update (U):
+@app.route('/create_flashcard', methods=['POST'])
+def create_flashcard():
+    data = request.get_json()
 
-selectedQuestion: The question selected for updating.
-updatedAnswer: The updated answer for the selected question.
-updatedTopic: The updated topic/category for the selected question.
+    if 'question' not in data or 'answer' not in data:
+        return jsonify({'error': 'Missing question or answer'}), 400
 
-Delete (D):
+    question = data['question']
+    answer = data['answer']
 
-deletedQuestion: The question selected for deletion.
-deletedAnswer: The corresponding answer for the deleted question.
-deletedTopic: The topic/category of the deleted question.
+    flashcard = {'question': question, 'answer': answer}
+    flashcards.append(flashcard)
+
+    return jsonify({'message': 'Flashcard created successfully'})
+
+@app.route('/get_flashcards', methods=['GET'])
+def get_flashcards():
+    return jsonify({'flashcards': flashcards})
+
+@app.route('/update_flashcard/<int:index>', methods=['PUT'])
+def update_flashcard(index):
+    if index < 0 or index >= len(flashcards):
+        return jsonify({'error': 'Invalid index'}), 400
+
+    data = request.get_json()
+
+    if 'question' not in data or 'answer' not in data:
+        return jsonify({'error': 'Missing question or answer'}), 400
+
+    flashcards[index]['question'] = data['question']
+    flashcards[index]['answer'] = data['answer']
+
+    return jsonify({'message': 'Flashcard updated successfully'})
+
+@app.route('/delete_flashcard/<int:index>', methods=['DELETE'])
+def delete_flashcard(index):
+    if index < 0 or index >= len(flashcards):
+        return jsonify({'error': 'Invalid index'}), 400
+
+    deleted_flashcard = flashcards.pop(index)
+
+    return jsonify({'message': 'Flashcard deleted successfully', 'deleted_flashcard': deleted_flashcard})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 
 
