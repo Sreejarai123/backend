0234071364a5b7a7108b8983a310558671e77ba1
Sreejarai123 <Sreeja.t.rai@gmail.com>
2023-10-20 08:50:57 -0700

    StudyBuddyCrud

--- model/study_buddy.py
+++ model/study_buddy.py
@@ -2,3 +2,31 @@
     @name.setter
     def name(self, name):
         self._name = name
+Create (C):
+newQuestion: A variable to store a new question being created.
+newAnswer: A variable to store the corresponding answer for the new question.
+newTopic: The topic or category to which the new question belongs.
+
+Read (R):
+
+questionsList: An array or list to store existing questions.
+answersList: An array or list to store corresponding answers.
+topicsList: An array or list to store the topics/categories of the questions.
+
+Update (U):
+
+selectedQuestion: The question selected for updating.
+updatedAnswer: The updated answer for the selected question.
+updatedTopic: The updated topic/category for the selected question.
+
+Delete (D):
+
+deletedQuestion: The question selected for deletion.
+deletedAnswer: The corresponding answer for the deleted question.
+deletedTopic: The topic/category of the deleted question.
+
+
+
+
+
+
